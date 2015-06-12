93f8a170d2c33a834b70b3e46e5a86ef896a12f5
Mathew Cha <dongkyun@mathews-mbp.attlocal.net>
2015-06-12 00:44:41 -0700

    I got some optimizations, more config options, general niceities

--- myChart.js
+++ myChart.js
@@ -1,46 +1,48 @@
 // Get the context of the canvas element we want to select
 var ctx = document.getElementById("myChart").getContext("2d");
 
-var data = {
+var initData = {
     labels: [],
     datasets: [
         {
             label: "pp",
             fillColor: "rgba(0,0,0,0)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
+            strokeColor: "rgba(127,21,55,1)",
+            pointColor: "rgba(127,21,55,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            pointHighlightStroke: "rgba(127,21,55,1)",
             data: []
         },
         {
             label: "pq",
             fillColor: "rgba(0,0,0,0)",
-            strokeColor: "rgba(101,137,155,1)",
-            pointColor: "rgba(101,137,155,1)",
+            strokeColor: "rgba(33,33,193,1)",
+            pointColor: "rgba(33,33,193,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
+            pointHighlightStroke: "rgba(33,33,193,1)",
             data: []
         },
         {
             label: "qq",
             fillColor: "rgba(0,0,0,0)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
+            strokeColor: "rgba(3,119,120,1)",
+            pointColor: "rgba(3,119,120,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
+            pointHighlightStroke: "rgba(3,119,120,1)",
             data: []
         }
     ]
 };
 
+var data = initData;
+
 var myLineChart = new Chart(ctx).Line(data, {
     bezierCurve: false,
     animationEasing: "easeOutQuint",
     animation: true,
-    animationSteps: 60,
-    pointDot : true
+    animationSteps: 10,
+    pointDot: true
 });
--- myScript.js
+++ myScript.js
@@ -1,4 +1,23 @@
+"use strict";
+
 var generations = [];
+var mateTimer;
+var timerBoolean = false;
+
+var addedGeneration = [];
+var removedGeneration = [0,0,0];
+
+function autoMate() {
+    if (!timerBoolean) {
+        mateTimer = setInterval(mateClick, 300);
+        timerBoolean = true;
+        document.getElementById("autorun").firstChild.data = "Turn off autorun";
+    } else {
+        clearInterval(mateTimer);
+        timerBoolean = false;
+        document.getElementById("autorun").firstChild.data = "Turn on autorun"
+    }
+}
 
 var Organism = function (allele1, allele2) {
     this.gene = [allele1, allele2];
@@ -6,14 +25,26 @@ var Organism = function (allele1, allele2) {
     if (this.gene[0] === "p" && this.gene[1] === "p") {
         this.phenotype = "dominant";
         this.genotype = "homozygous";
-    } else if (this.gene[0] === "p" && this.gene[1] === "q" || this.gene[0] === "q" && this.gene[1] === "p") {
+    } else if ((this.gene[0] === "p" && this.gene[1] === "q") || (this.gene[0] === "q" && this.gene[1] === "p")) {
         this.phenotype = "dominant";
         this.genotype = "heterozygous";
-    } else if (this.gene[0] === "q" && this.gene[1] === "q") {
+    } else { // if (this.gene[0] === "q" && this.gene[1] === "q") {
         this.phenotype = "recessive";
         this.genotype = "homozygous";
     }
-}
+};
+
+Organism.prototype.returnGenotype = function () {
+    if (this.phenotype === "dominant") {
+        if (this.genotype === "homozygous") {
+            return "pp";
+        } else { // genotype === "heterozygous"
+            return "pq";
+        }
+    } else { //if (this.phenotype === "recessive") {
+        return "qq";
+    }
+};
 
 var Generation = function (_organisms) {
     this.organisms = _organisms;
@@ -23,88 +54,225 @@ var Generation = function (_organisms) {
     this.pq = 0;
     this.qq = 0;
 
-    for (i = 0; i < this.populationSize; i++) {
-        if (this.organisms[i].phenotype === "dominant") {
-            if (this.organisms[i].genotype === "homozygous") {
-                this.pp++;
-            } else {  // organisms[i].genotype === "heterozygous"
-                this.pq++;
-            }
-        } else { // organisms[i].phenotype === "recessive"
+    for (var i = 0; i < this.populationSize; i++) {
+        switch (this.organisms[i].returnGenotype()) {
+        case "pp":
+            this.pp++;
+            break;
+        case "pq":
+            this.pq++;
+            break;
+        case "qq":
             this.qq++;
+            break;
+        default:
+            console.log("There's literally no way you got here. How the hell.");
+            break;
         }
     }
 
-    this.p  = (2*this.pp + this.pq) / (2 * this.populationSize);
-    this.q  = (2*this.qq + this.pq) / (2 * this.populationSize);
+    this.p = (2 * this.pp + this.pq) / (2 * this.populationSize);
+    this.q = (2 * this.qq + this.pq) / (2 * this.populationSize);
     this.ppPercent = this.pp / this.populationSize;
     this.pqPercent = this.pq / this.populationSize;
     this.qqPercent = this.qq / this.populationSize;
+};
 
-    this.mate = function (childrenAmount) {
-        var childArray = [];
-        for (var i = 0; i < childrenAmount; i++) {
-            var father = this.organisms[Math.floor(Math.random() * this.populationSize)];
-            var mother = this.organisms[Math.floor(Math.random() * this.populationSize)];
+Generation.prototype.mate = function (childrenAmount) {
+    migrate();
+    var childArray = addedGeneration;
+    var x = 0;
+    for (var i = 0; i < childrenAmount; i++) {
+        var father = this.organisms[Math.floor(Math.random() * this.populationSize)];
+        var mother = this.organisms[Math.floor(Math.random() * this.populationSize)];
 
-            var fatherAllele = father.gene[Math.round(Math.random())];
-            var motherAllele = mother.gene[Math.round(Math.random())];
+        var fatherAllele = father.gene[Math.round(Math.random())];
+        var motherAllele = mother.gene[Math.round(Math.random())];
 
-            childArray.push(new Organism(fatherAllele, motherAllele));
+        var pqMutateRate = parseFloat(document.getElementById("p-q-mutate").value);
+        var qpMutateRate = parseFloat(document.getElementById("q-p-mutate").value);
+
+        var pqChance = Math.random();
+        var qpChance = Math.random();
+
+        if (fatherAllele === "p" && pqChance < pqMutateRate) {
+            fatherAllele = "q";
+        } else if (qpChance < qpMutateRate) { // fatherAllele === "q"
+            fatherAllele = "p";
         }
 
-        return new Generation(childArray);
-    }
-}
+        if (motherAllele === "p" && pqChance < pqMutateRate) {
+            motherAllele = "q";
+        } else if (qpChance < qpMutateRate) { // fatherAllele === "q"
+            motherAllele = "p";
+        }
 
-function round(n, d) {
-    return Math.round(Math.pow(10, d) * n) / Math.pow(10, d);
-}
+        var child = new Organism(fatherAllele, motherAllele);
+        var killRate;
+        var survivalRate = Math.random();
+        var emigrationQueueID;
+        
+
+        switch (child.returnGenotype()) {
+        case "pp":
+            killRate = parseFloat(document.getElementById("pp-survival").value);
+            emigrationQueueID = 0;
+            break;
+        case "pq":
+            killRate = parseFloat(document.getElementById("pq-survival").value);
+            emigrationQueueID = 1;
+            break;
+        case "qq":
+            killRate = parseFloat(document.getElementById("qq-survival").value);
+            emigrationQueueID = 2;
+            break;
+        default:
+            console.log("There's literally no way you got here. How the hell.");
+            killRate = -1;
+            break;
+        }
+        
+        if (survivalRate < killRate) {
+            // It lives!
+            //childArray.push(child);
+            if (removedGeneration[emigrationQueueID] < 1) {
+                childArray.push(child);
+            } else {
+                removedGeneration[emigrationQueueID]--;
+                i--;   
+            }
+        } else {
+            i--;
+            x++;
+            
+            if (x > this.organisms.length*200) {
+                alert("Hm... It looks like natural selection was too hard on this species. Everyone died. Exiting program.");
+                document.getElementById("mate").disabled = true;
+                document.getElementById("autorun").disabled = true;
+                
+                if (timerBoolean) {
+                    autoMate();
+                }
+                
+                return this.organisms;
+            }
+        }
+
+
+    }
+    addedGeneration = [];
+    removedGeneration = [0,0,0];
+    return new Generation(childArray);
+};
 
 function init() {
     var initPP = parseInt(document.getElementById("initPP").value);
     var initPQ = parseInt(document.getElementById("initPQ").value);
     var initQQ = parseInt(document.getElementById("initQQ").value);
-    var childAmount = parseInt(document.getElementById("childAmount").value);
+    var childAmount = initPP + initPQ + initQQ;
 
     var parents = [];
 
     for (var i = 0; i < initPP; i++) {
         parents.push(new Organism("p", "p"));
     }
-    for (var i = 0; i < initPQ; i++) {
+    for (i = 0; i < initPQ; i++) {
         parents.push(new Organism("p", "q"));
     }
-    for (var i = 0; i < initQQ; i++) {
+    for (i = 0; i < initQQ; i++) {
         parents.push(new Organism("q", "q"));
     }
 
     generations[0] = new Generation(parents);
-
-    //document.getElementById("generations").innerHTML = dataToString(0);
-    myLineChart.addData([initPP / childAmount, initPQ / childAmount, initQQ / childAmount], "");//
-    generations.push(generations[0].mate(childAmount));
-
-    //document.getElementById("generations").innerHTML += dataToString(1);
-    myLineChart.addData([generations[1].ppPercent, generations[1].pqPercent, generations[1].qqPercent], "");//, "1");
-}
-
-function dataToString(n) {
-    var formatString = "Generation " + n + ": <br>" +
-        "p: " + round(generations[n].p, 2) + "<br>" +
-        "q: " + round(generations[n].q, 2) + "<br><br>" +
-        "Homozygous dominant: " + round(generations[n].pp, 2) + "<br>" +
-        "Heterozygous dominant: " + round(generations[n].pq, 2) + "<br>" +
-        "Homozygous recessive: " + round(generations[n].qq, 2) + "<br><br>";
-    return formatString;
+    myLineChart.addData([initPP / childAmount, initPQ / childAmount, initQQ / childAmount], "");
 }
 
 function mateClick() {
-    var parent = generations[generations.length-1];
-    var childAmount = parseInt(document.getElementById('childAmount').value);
+    var childAmount = parseInt(document.getElementById('childAmount').value) +
+                      parseInt(document.getElementById("pp-gen-drift").value) +
+                      parseInt(document.getElementById("pq-gen-drift").value) +
+                      parseInt(document.getElementById("qq-gen-drift").value);
+    document.getElementById('childAmount').value = childAmount;
+    
+    if (childAmount <= 0) {
+        alert("Everyone died! End of simulation.");
+        document.getElementById("mate").disabled = true;
+        document.getElementById("autorun").disabled = true;
+        if (timerBoolean) {
+            autoMate();
+        }
+        return;
+    }
+    
+    if (generations.length === 0) {
+        init();
+        document.getElementById("initial").style.display = "none";
+        document.getElementById("pop").style.display = "block";
+        document.getElementById("initPP").disabled = true;
+        document.getElementById("initPQ").disabled = true;
+        document.getElementById("initQQ").disabled = true;
+        
+        document.getElementById('childAmount').value = parseInt(document.getElementById("initPP").value) + 
+                      parseInt(document.getElementById("initPQ").value) + 
+                      parseInt(document.getElementById("initQQ").value);
+    }
+    var parent = generations[generations.length - 1];
     generations.push(parent.mate(childAmount));
-    //document.getElementById("generations").innerHTML += dataToString(generations.length - 1);
 
-    var children = generations[generations.length-1];
-    myLineChart.addData([children.ppPercent, children.pqPercent, children.qqPercent], "");//generations.length - 1);
+    var children = generations[generations.length - 1];
+    myLineChart.addData([children.ppPercent, children.pqPercent, children.qqPercent], "");
+    
+    document.getElementById("genCounter").innerHTML = generations.length - 1;
+}
+
+function clearGenerations() {
+    if (timerBoolean) {
+        autoMate();
+    }
+    
+    for (var i = 0; i < generations.length; i++) {
+        myLineChart.removeData();
+    }
+    generations = [];
+    
+    document.getElementById("initial").style.display = "block";
+    document.getElementById("pop").style.display = "none";
+    
+    document.getElementById("initPP").disabled = false;
+    document.getElementById("initPQ").disabled = false;
+    document.getElementById("initQQ").disabled = false;
+    
+    document.getElementById("mate").disabled = false;
+    document.getElementById("autorun").disabled = false;
+    
+    document.getElementById("genCounter").innerHTML = 0;
+}
+
+function migrate() {
+    var ppGroup = parseInt(document.getElementById("pp-gen-drift").value)
+    if (ppGroup >= 0) {
+        for (var i = 0; i < ppGroup; i++) {
+            addedGeneration.push(new Organism("p", "p"));
+        }
+    } else {
+        removedGeneration[0] = Math.min(-ppGroup, generations[generations.length-1].pp);
+    }
+    
+    var pqGroup = parseInt(document.getElementById("pq-gen-drift").value)
+    if (pqGroup >= 0) {
+        for (var i = 0; i < pqGroup; i++) {
+            addedGeneration.push(new Organism("p", "q"));
+        }
+    } else {
+        removedGeneration[1] = Math.min(-pqGroup, generations[generations.length-1].pq);
+    }
+    
+    var qqGroup = parseInt(document.getElementById("qq-gen-drift").value)
+    if (qqGroup >= 0) {
+        for (var i = 0; i < qqGroup; i++) {
+            addedGeneration.push(new Organism("q", "q"));
+        }
+    } else {
+        removedGeneration[2] = Math.min(-qqGroup, generations[generations.length-1].qq);
+    }
 }
